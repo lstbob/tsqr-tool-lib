0632b6dc997817b09d9be452e0c7fcc7ae750a63
lstbob <bobchevlyuben@gmail.com>
2025-11-06 18:20:56 +0200

    mes

--- src/TSQR.ToolLibrary.Application/Exceptions/ApplicationExceptionHandler.cs
+++ src/TSQR.ToolLibrary.Application/Exceptions/ApplicationExceptionHandler.cs
@@ -1,9 +1,10 @@
 namespace TSQR.ToolLibrary.Application.Exceptions;
 
 public class ApplicationExceptionHandler : IPipelineBehavior<TRequest, TResult>
-    where TRequest is IRequest, TResult is IResult;
+    where TRequest is IRequest, eResult is IResult;
 {
    
+    testetsetstetsetset
    // TODO: implement application exception handler that is not part of web pipeline
    // e.g. http handlers middlewares etc.
 }
